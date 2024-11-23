4c3c37fa840183c30afa62094dfd920681d0e682
Kwok Lau <kslau07@protonmail.com>
2024-11-23 17:31:58 -0500

    refactor: Merge methods, use private fields

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,7 +1,7 @@
 // Gameboard.js
 
 export default class Gameboard {
-  static buildGrid() {
+  static #buildGrid() {
     const gridArray = [];
     const numRows = 10;
     const numCols = 10;
@@ -19,72 +19,66 @@ export default class Gameboard {
   }
 
   constructor() {
-    this.grid = Gameboard.buildGrid();
+    this.grid = Gameboard.#buildGrid();
+    this.ships = [];
   }
 
   getGrid() {
     return this.grid;
   }
 
-  isValidCoordinates(coords) {
-    const [colIndex, rowIndex] = coords;
-    const maxIndex = 9;
-
-    if (colIndex > maxIndex) return false;
-    if (rowIndex > maxIndex) return false;
-
-    return true;
-  }
-
-  isValidCell(nextCell) {
-    if (nextCell === undefined) return 'Out of bounds';
-
-    if (nextCell.ship !== 'none')
-      return 'Cell is already occupied by another ship.';
-
-    return true;
-  }
-
-  // Check ship placement cells are in-bounds and unoccupied
-  isValidPlacement(ship, coords, orientation) {
-    if (!this.isValidCoordinates(coords)) return false;
-
-    const { grid } = this;
-
-    let nextCell;
-    for (let i = 0; i < ship.length; i += 1) {
-      if (orientation === 'vertical') {
-        nextCell = grid[coords[0] + i]?.[coords[1]]; // Use optional chaining operator (?.) to try to access next hypothetical cell
-      } else if (orientation === 'horizontal') {
-        nextCell = grid[coords[0]]?.[coords[1] + i];
-      }
-
-      if (this.isValidCell(nextCell) !== true) {
-        return false;
-      }
+  // Check cell and throw relevant error
+  #validateCell(cell) {
+    if (cell === undefined) {
+      throw new RangeError('Out of bounds');
+    } else if (cell.ship !== 'none') {
+      throw new Error('Cell is already occupied by another ship.');
     }
-
-    return true;
   }
 
-  performPlacement(ship, coords, orientation) {
+  *#getNextCell(ship, coords, orientation) {
     const { grid } = this;
 
-    let nextCell;
     for (let i = 0; i < ship.length; i += 1) {
       if (orientation === 'vertical') {
-        nextCell = grid[coords[0] + i][coords[1]];
+        yield grid[coords[0] + i]?.[coords[1]]; // Use optional chaining operator (?.) to try to access next hypothetical cell
       } else if (orientation === 'horizontal') {
-        nextCell = grid[coords[0]][coords[1] + i];
+        yield grid[coords[0]]?.[coords[1] + i];
       }
-
-      nextCell.ship = ship;
     }
   }
 
   placeShip(ship, coords, orientation) {
-    if (this.isValidPlacement(ship, coords, orientation)) {
-      this.performPlacement(ship, coords, orientation);
+    const gridDeepClone = JSON.parse(JSON.stringify(this.grid));
+    const generator = this.#getNextCell(ship, coords, orientation);
+
+    try {
+      for (let nextCell of generator) {
+        this.#validateCell(nextCell);
+        nextCell.ship = ship;
+      }
+    } catch ({ name, message }) {
+      console.log(`${name}: ${message}`);
+      this.grid = gridDeepClone; // Revert grid when error thrown
     }
+  }
+
+  receiveAttack(coords) {
+    const targetCell = this.grid[coords[0]][coords[1]];
+    const { attacked } = targetCell;
+
+    if (attacked === true) {
+      throw new Error('You have already attacked this cell!');
+    }
+
+    if (targetCell.ship !== 'none') {
+      targetCell.ship.hit();
+    }
+
+    targetCell.attacked = true;
+  }
+
+  allSunk() {
+    return true;
   }
 }
