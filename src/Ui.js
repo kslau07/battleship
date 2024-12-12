93ea74f29fb01d5d4beaee487b91c3d85ae85db5
Kwok Lau <kslau07@protonmail.com>
2024-12-11 22:53:08 -0500

    feat: Create grid cell buttons, allow attacking
    style: Show 4 game grids (usable)

--- src/Ui.js
+++ src/Ui.js
@@ -4,50 +4,114 @@ import './global.css';
 import './style.css';
 import Game from './Game';
 
-const foo = document.querySelector('#new-game-button');
+const createOwnCell = (cellObj) => {
+  const cellDiv = document.createElement('div');
+  cellDiv.className = 'cell own';
+  cellDiv.textContent = cellObj.ship === 'none' ? '' : cellObj.ship.getName();
+  return cellDiv;
+};
 
-const createCellElement = (cellObj) => {
-  const cellEl = document.createElement('div');
-  cellEl.className = 'cell';
-  cellEl.textContent = cellObj.ship === 'none' ? '' : cellObj.ship.getName();
+const attackAndUpdateCell = (gameObj, playerObj, cellObj, cellBtn) => {
+  cellObj.attack();
+
+  if (cellObj.isAttacked() === true && cellObj.ship === 'none') {
+    cellBtn.textContent = 'miss';
+  } else if (cellObj.isAttacked() === true && cellObj.ship !== 'none') {
+    cellBtn.textContent = 'hit!';
+    cellBtn.style.background = 'red';
+  }
+
+  cellBtn.disabled = true;
+
+  // TODO: Check end of game
+};
+
+const createEnemyCell = (gameObj, playerObj, cellObj) => {
+  const cellBtn = document.createElement('button');
+  cellBtn.className = `cell cell-${cellObj.rowIndex}-${cellObj.columnIndex}`;
+  cellBtn.textContent = '';
+  cellBtn.addEventListener('click', () => {
+    attackAndUpdateCell(gameObj, playerObj, cellObj, cellBtn);
+  });
+  return cellBtn;
+};
+
+const createCellElement = (gameObj, playerObj, side, cellObj) => {
+  let cellEl;
+
+  if (side === 'own') {
+    cellEl = createOwnCell(cellObj);
+  } else if (side === 'enemy') {
+    cellEl = createEnemyCell(gameObj, playerObj, cellObj);
+  }
+
   return cellEl;
 };
 
-const createRowElement = (rowObj) => {
+const createRowElement = (gameObj, playerObj, side, rowObj) => {
   const rowEl = document.createElement('div');
-  rowEl.className = 'row';
+  rowEl.classList.add('row');
+
   rowObj.forEach((cellObj) => {
-    const cellEl = createCellElement(cellObj);
+    const cellEl = createCellElement(gameObj, playerObj, side, cellObj);
+    // cellEl.classList.add(`column-${columnIndex}`);// FIXME: ADD COORDS INFO AS CLASS
     rowEl.appendChild(cellEl);
   });
   return rowEl;
 };
 
-const createGridElement = (playerGrid) => {
+const createGridElement = (gameObj, playerObj, side) => {
+  const playerGrid = playerObj.getGameboard().getGrid();
   const gridEl = document.createElement('div');
   gridEl.className = 'grid';
 
-  playerGrid.forEach((row) => {
-    const rowEl = createRowElement(row);
+  playerGrid.forEach((rowObj, rowIndex) => {
+    const rowEl = createRowElement(gameObj, playerObj, side, rowObj);
+    rowEl.classList.add(`row-${rowIndex}`);
+
     gridEl.appendChild(rowEl);
   });
 
   return gridEl;
 };
 
-const createGame = () => {
-  const game = new Game();
-  const playerGrid = game.getPlayer1().getGameboard().getGrid();
-  const gridContainer = document.querySelector('.grid-container');
-  const gridEl = createGridElement(playerGrid);
-  gridContainer.appendChild(gridEl);
+const populateGameboard = (gameObj, playerObj, side) => {
+  const gridContainer = document.getElementById(
+    `${playerObj.getName()}-${side}-grid`,
+  );
+
+  let gridElement;
+  if (side === 'own') {
+    gridElement = createGridElement(gameObj, playerObj, side);
+  } else if (side === 'enemy') {
+    const enemyObj = gameObj.getOppositePlayer(playerObj);
+    gridElement = createGridElement(gameObj, enemyObj, side);
+  }
+
+  gridContainer.appendChild(gridElement);
 };
 
-foo.addEventListener('click', createGame);
+const populateGrids = (gameObj) => {
+  const players = gameObj.getPlayers();
+  players.forEach((player) => {
+    populateGameboard(gameObj, player, 'own');
+    populateGameboard(gameObj, player, 'enemy');
+  });
+};
 
-// FIXME: DELETE ME, DEVELOPMENT TOOL
-// Press 't' to invoke createGame()
+const createGame = () => {
+  const game = new Game();
+  const player1 = game.getPlayer1();
+  const player2 = game.getPlayer2();
+  populateGrids(game);
+};
+
+const createGameButton = document.querySelector('#new-game-button');
+createGameButton.addEventListener('click', createGame);
+
 document.onkeyup = function () {
+  // FIXME: DELETE ME, DEVELOPMENT TOOL
+  // Press 't' to invoke createGame()
   var e = e || window.event; // For IE to cover IEs window event-object
   if (e.which == 84) {
     createGame();
@@ -55,5 +119,4 @@ document.onkeyup = function () {
   }
 };
 
-// FIXME: DELETE ME, DEVELOPMENT TOOL
-createGame();
+// createGame(); // FIXME: DELETE ME, DEVELOPMENT TOOL
